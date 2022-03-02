58460f8e66bba175492ebd0fc46335f1aedf3574
nakaa <nana.jikia21b@geolab.edu.ge>
2022-03-02 17:48:11 +0400

    add changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const APP_ID = 'c0fdd29b';
+  const APP_KEY = '630247090761d62d96b925e55226556d'
+  const exampleReq = `https://retoolapi.dev/FNoduP/products`
+  return (
+    <div className="App">
+      <h1>Hello react!</h1>
+    </div>
+  );
+}
+
+export default App;
